3199aa95725b74dcb8eeddf60e8b61931e8f0348
Laura Sofia <laura.sp.silva2008@gmail.com>
2026-09-03 14:59:17 -0300

    Variáveis e useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react"
+
 function App() {
+
+  let [cliques, setCliques] = useState(0)
+  let [nome, setNome] = useState("Aguardando...")
+
+
+  function carregarNome(){
+    setNome("Laura")
+  }
+
 
 
   return (
     <div>
       <h1>Hello World!</h1>
       <p>Estou aprendendo React :) </p>
+      <p> Meu nome é: {nome}</p>
+      <button onClick={() => setNome ("Laura")}>Carregar nome </button>
+
+      <hr />
+
+      <p>Você clicou <strong>{cliques}</strong> vezes</p>
+      <button onClick= {() => setCliques (cliques + 1 )}>Clique aqui</button>
+      {/*
+                      () => : Arrow Function -> Função do tipo seta (lambda)
+                      Função anônima
+      */}
+
     </div>
   )
 }
